ba634ddfb6948bcb3e48edc19db47d7b6d820a52
yoonjookim <yjblossom@gmail.com>
2024-04-29 18:40:35 -0400

    Debug 'parsedTodos' in TodoList.jsx

--- Project_To_Do_List/src/components/TodoList.jsx
+++ Project_To_Do_List/src/components/TodoList.jsx
@@ -1,17 +1,19 @@
 import TodoListItem from "./TodoListItem";
 
 export default function TodoList(props) {
-    const {todos, toggleTodo, deleteTodo} =props;
+    const { todos, toggleTodo, deleteTodo } = props;
 
-    const parsedTodos = Object.values(todos).map(todo => (
-        <TodoListItem 
-            key={todo.id} 
-            {...todo} 
-            toggleTodo={() => toggleTodo(todo.id)} 
-            deleteTodo={() => deleteTodo(todo.id)}/>
-    )); 
-//toggleTodo={() => toggleTodo(todo.id)} means, 
-//when todoListItem will call toggleTodo, I want to run toggleTodo with todo.id
+    const parsedTodos =
+        todos &&                               //modify: Can you parse the todos if you actually have todos. If you don't, don't do it.
+        Object.values(todos).map(todo => (
+            <TodoListItem
+                key={todo.id}
+                {...todo}
+                toggleTodo={() => toggleTodo(todo.id)}
+                deleteTodo={() => deleteTodo(todo.id)} />
+        ));
+    //toggleTodo={() => toggleTodo(todo.id)} means, 
+    //when todoListItem will call toggleTodo, I want to run toggleTodo with todo.id
     return (
         <section className="TodoList">
             <h1>All todos</h1>
