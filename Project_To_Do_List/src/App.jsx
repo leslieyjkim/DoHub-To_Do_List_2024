a2458de0ff51ba6ae34ae97050a1e31551e537ee
yoonjookim <yjblossom@gmail.com>
2024-04-29 14:12:41 -0400

    Set up component of Footer

--- Project_To_Do_List/src/App.jsx
+++ Project_To_Do_List/src/App.jsx
@@ -1,19 +1,18 @@
- import './App.scss'
- import Header from "./components/Header";
+import './App.scss'
+import Footer from './components/Footer';
+import Header from "./components/Header";
 import TodoForm from './components/TodoForm';
 import TodoList from './components/TodoList';
 
 function App() {
   return (
     <>
-    <Header />
-    <main>
-      <TodoForm />
-      <TodoList />
-    </main>
-    <footer>
-      <p>All rights reserved 2024 MUHAHA</p>
-    </footer>
+      <Header />
+      <main>
+        <TodoForm />
+        <TodoList />
+      </main>
+      <Footer />
     </>
   )
 }
---
+++ Project_To_Do_List/src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer(props) {
+    return (
+        <footer>
+            <p>All rights reserved 2024 MUHAHA</p>
+        </footer>
+    );
+}
