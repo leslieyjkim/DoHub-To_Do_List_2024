57638e00413b6b39531e21fe94dafe6a94a737d4
yoonjookim <yjblossom@gmail.com>
2024-04-29 13:08:17 -0400

    Initial HTML Skeleton in App.jsx

--- Project_To_Do_List/src/App.jsx
+++ Project_To_Do_List/src/App.jsx
@@ -1,10 +1,37 @@
  import './App.css'
 
 function App() {
-
-
   return (
     <>
+    <header className="Header">
+      <h1>Super Todo List of Destiny</h1>
+    </header>
+    <main>
+      <section className="TodoForm">
+        <button>Add a task</button>
+        <form>
+          <input type="text" name="task" placeholder="Enter the task"/>
+          <input type="color" name="color" placeholder="Enter the color"/>
+          <input type="date" name="dueDate" placeholder="Enter the due date"/>
+          <button>Add to-do</button>
+        </form>
+      </section>
+      <section className="TodoList">
+        <h1>All todos</h1>
+        <ul>
+          <li>
+            <span>task</span>
+            <span>date</span>
+            <button>Mark as complete</button>
+            <button>Edit</button>
+            <button>Delete</button>
+          </li>
+        </ul>
+      </section>
+    </main>
+    <footer>
+      <p>All rights reserved 2024 MUHAHA</p>
+    </footer>
     </>
   )
 }
